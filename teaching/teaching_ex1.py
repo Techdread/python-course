efca9b0c32ef208466fb2587719c61ba0172a32c
Peter <Techdread@gmail.com>
2018-06-06 23:12:05 +0100

    Added close word match

--- teaching/teaching_ex1.py
+++ teaching/teaching_ex1.py
@@ -1,5 +1,5 @@
 import json
-
+from difflib import get_close_matches
 """
 This script imports a json with a dictionary of words
 """
@@ -11,9 +11,17 @@ def get_definition(word):
     """This function takes a word and returns its defnition"""
     word = word.lower()
     if word in word_dict:
-        return word_dict[word] 
+        return word_dict[word]
+    elif len(get_close_matches(word, word_dict.keys())) > 0:
+        close_word = get_close_matches(word, word_dict.keys())[0]
+        question = input("Did you mean %s instead? Enter a Y (for Yes) or an N (for No) " % close_word)
+        if question.lower() == 'y':
+            return word_dict[close_word]
+        else:
+            return ""
     else:
         return "Word is not in dictionary!"
+
 
 word = input("What word do you want to look up?")
 print(get_definition(word))
